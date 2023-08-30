c95ae1c4b94ae69bbcee50a2db11b5db0a906ced
josejsjsjs <joserodiguesfs@gmail.com>
2023-08-29 23:49:29 -0300

    29/08/2023

---
+++ pizzaria-da-nonna06/pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+  {
+      id: 1,
+      name: 'CONTRA FERRUGEM',
+      img: 'images/suvinil-esmalte-2.webp',
+      price: [139.99, 139.99, 139.99],
+      sizes: [
+        'BRANCO',
+        'AZUL',
+        'VERDE'
+      ],
+      description: 'TINTA ESMALTE CONTRA FERRUGEM BRILHANTE BRANCA 3,6L SUVINIL'
+    },
+    {
+      id: 2,
+      name: 'PISOS',
+      img: 'images/suvinil-esmalte-3.webp',
+      price: [21.00, 24.00, 26.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'TINTA PISO CERÂMICA 3,6L SUVINIL'
+    },
+    {
+      id: 3,
+      name: 'SEMPRE BRANCO',
+      img: 'images/suvinil-esmalte-4.webp',
+      price: [23.00, 26.00, 28.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'TINTA ESMALTE SEMPRE BRANCO ACETINADO BRANCA 3,6L SUVINIL'
+    },
+    {
+      id: 4,
+      name: 'MADEIRA PROTEGIDA',
+      img: 'images/suvinil-esmalte-5.webp',
+      price: [25.00, 28.00, 30.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'VERNIZ MARÍTIMO MADEIRA PROTEGIDA NATURAL 3,6L SUVINILolho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    },
+    {
+      id: 5,
+      name: 'Portuguesa',
+      img: 'images/suvinil-esmalte-6.webp',
+      price: [24.00, 27.00, 29.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+    },
+    {
+      id: 6,
+      name: 'Moda da Casa',
+      img: 'images/pizza6.png',
+      price: [30.00, 33.00, 35.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+    },
+    {
+      id: 7,
+      name: 'Banana com canela',
+      img: 'images/pizza7.png',
+      price: [27.00, 30.00, 32.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Mussarela, banana, canela e açúcar'
+    },
+    {
+      id: 8,
+      name: 'Chocolate com morango',
+      img: 'images/pizza8.png',
+      price: [30.00, 32.00, 35.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Creme de leite, lascas de chocolate e morangos'
+    }
+];
